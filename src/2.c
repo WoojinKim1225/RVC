cbbc1f8bfc3daeca13cda0cd7e60f4a26a82aa8e
WoojinKim1225 <96871618+WoojinKim1225@users.noreply.github.com>
2025-11-18 13:49:53 +0900

    added structural init files

---
+++ src/2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <stdbool.h>
+
+// Controller
+void Wheel_Controller();
+void Cleaner_Control();
+
+int Det_OL();
+int Det_DE();
+// 인터페이스
+void Front_SI();
+void Left_SI();
+void Right_SI();
+void Dust_SI();
+
+//동작
+void MoveForward();
+void TurnLeft();
+void TurnRight();
+void MoveBackward();
+void Cleaner(const char *mode);
+// sensor input
+bool ReadFrontSensor();
+int ReadLeftSensor();
+int ReadRightSensor();
+int ReadDustSensor();
+
+typedef struct {
+    bool F;   
+    bool L;   
+    bool R;  
+    bool D; 
+} SensorData;
+enum MotorCommand {
+    MOVE_FWD, MOVE_BACK, TURN_LEFT, TURN_RIGHT, STOP 
+};
+enum CleanerMommand {
+    OFF, ON, UP
+};
+
+void main()
+{
+    int obstacle_Location;
+    bool dust_Existence;
+
+    while(1)
+    {
+        obstacle_Location = Det_OL();
+        dust_Existence = Det_DE();
+
+        wait(200);
+    }
+}
