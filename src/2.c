5372a9a16d79919d2bd49d23dff46c15550abe30
WoojinKim1225 <96871618+WoojinKim1225@users.noreply.github.com>
2025-11-18 15:02:05 +0900

    added cleaner control state machine

--- src/2.c
+++ src/2.c
@@ -6,7 +6,7 @@
 
 // Controller
 void WheelController();
-void CleanerControl();
+CleanerState CleanerControl(CleanerState, bool, bool);
 
 int DetermineObstacleLocation();
 int DetermineDustExistence();
@@ -21,7 +21,10 @@ void MoveForward();
 void TurnLeft();
 void TurnRight();
 void MoveBackward();
-void Cleaner(const char *mode);
+void CleanerOff();
+void CleanerOn();
+void CleanerUp();
+
 // sensor input
 bool ReadFrontSensor();
 int ReadLeftSensor();
@@ -34,23 +37,71 @@ typedef struct {
     bool R;  
     bool D; 
 } SensorData;
+
+
+typedef enum {
+    CLEANER_OFF,
+    CLEANER_ON,
+    CLEANER_UP
+} CleanerState;
+
 enum MotorCommand {
     MOVE_FWD, MOVE_BACK, TURN_LEFT, TURN_RIGHT, STOP 
 };
+
 enum CleanerCommand {
     OFF, ON, UP
 };
 
-void main()
+void 
+main()
 {
     int obstacle_location;
     bool dust_existence;
+
+    bool cleaner_control_enable;
+    CleanerState cleaner_state = CLEANER_OFF;
 
     while(1)
     {
         obstacle_location = DetermineObstacleLocation();
         dust_existence = DetermineDustExistence();
+        
+        cleaner_state = CleanerControl(cleaner_state, dust_existence, cleaner_control_enable);
 
         wait(TICK);
+    }
+}
+
+CleanerState
+CleanerControl(CleanerState cleaner_state, bool dust_existence, bool cleaner_control_enable)
+{
+    switch (cleaner_state) 
+    {
+        case CLEANER_OFF:
+            if (cleaner_control_enable) {
+                CleanerOn();
+                cleaner_state = CLEANER_ON;
+            }
+        break;
+        case CLEANER_ON:
+            if (dust_existence) {
+                CleanerUp();
+                cleaner_state = CLEANER_UP;
+            } else if (!cleaner_control_enable) {
+                CleanerOff();
+                cleaner_state = CLEANER_OFF;
+            }
+            break;
+        break;
+        case CLEANER_UP:
+            if (!dust_existence) {
+                CleanerOn();
+                cleaner_state = CLEANER_ON;
+            } else if (!cleaner_control_enable) {
+                CleanerOff();
+                cleaner_state = CLEANER_OFF;
+            }
+        break;
     }
 }
